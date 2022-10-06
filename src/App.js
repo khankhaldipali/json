0307945033e922f4475179c1a9f6b41953d80b11
your_username <khankhaldipali007@gmail.com>
2022-10-06 22:25:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useEffect, useState } from 'react';
+const axios = require('axios');
+
 
 function App() {
+  const [char, setchar] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://dummyjson.com/products')
+      .then(function (res) {
+        console.log(res.data.products);
+        setchar(res.data.products);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className='row'>
+        {
+          char.map((item) => {
+            return (
+              <>
+                <div className='col-12'>
+                  <div className='flex main'>
+                    <div className='w-30'>
+                      <h3 className='title'>{item.title}</h3>
+                      <h4>Category:{item.category}</h4>
+                      <img src={item.thumbnail} alt="" />
+                    </div>
+                    <div className='w-70'>
+                      <h6 className='main_dis'><h3 className='discri'>Description:</h3>{item.description}</h6>
+                      <div className='flex'>
+                        <h4 className='price'>Price:{item.price}/-</h4>
+                        <h5 className='dis'>Discount:{item.discountPercentage}%</h5>
+                      </div>
+                      <h5>Rating:{item.rating}</h5>
+                      <h4 className='brand'>Brand:{item.brand}</h4>
+                      <h5>Stock:{item.stock}</h5>
+                      <div className='flex'>
+                        <button className='btn1'>Buy now</button>
+                        <button className='btn2'>Add to cart</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          })}
+      </div>
     </div>
   );
 }
-
 export default App;
